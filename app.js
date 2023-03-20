0a11b6444d63384f05552be2b841f869089ec12d
EddEnt <61289536+EddEnt@users.noreply.github.com>
2023-03-19 23:17:11 -0400

    Final commit
    Final commit of code for due date. After Anjana has marked, continue to design & improve for jobs.

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 const hamburgerButton = document.getElementById("hamburger");
 const navList = document.getElementById("navList");
+let topBTN = document.getElementById("topBTN");
 
 function toggleButton() {
   navList.classList.toggle("show");
 }
 
 hamburgerButton.addEventListener("click", toggleButton);
+
+// When the user scrolls down, display the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    topBTN.style.display = "block";
+  } else {
+    topBTN.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
